4e441b32abcea8b1500bf03efe1110c847b138a7
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-05-14 06:31:28 -0300

    first commit

---
+++ src/main/java/com/infosaude/infosaude/InfosaudeApplication.java
@@ -0,0 +1,13 @@
+package com.infosaude.infosaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfosaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfosaudeApplication.class, args);
+	}
+
+}
