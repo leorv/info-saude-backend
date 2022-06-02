99f43ee881b9c0e881a7b24344a0de7e96b79d68
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-06-01 23:35:33 -0300

    vaccine-takens crud, controller and service. ok

--- src/main/java/com/infosaude/infosaude/controllers/VaccineTakenController.java
+++ src/main/java/com/infosaude/infosaude/controllers/VaccineTakenController.java
@@ -1,5 +1,18 @@
 package com.infosaude.infosaude.controllers;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import com.infosaude.infosaude.entities.VaccineTaken;
+import com.infosaude.infosaude.services.VaccineTakenService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +20,55 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/vaccines-taken")
 public class VaccineTakenController {
     
+    public VaccineTakenService vaccinetakenService;
+
+    public VaccineTakenController(VaccineTakenService vaccinetakenService) {
+        this.vaccinetakenService = vaccinetakenService;
+    }
+
+    @PostMapping("/create")
+    public String createVaccineTaken(@RequestBody VaccineTaken vaccinetaken) throws InterruptedException, ExecutionException {
+        return vaccinetakenService.createVaccineTaken(vaccinetaken);
+    }
+
+    @GetMapping("/")
+    public List<VaccineTaken> getAllVaccineTakens() throws InterruptedException, ExecutionException {
+        return vaccinetakenService.getAllVaccineTakens();
+    }
+
+    @GetMapping("/{id}")
+    public VaccineTaken getVaccineTakenById(@PathVariable(value = "id") String id) throws InterruptedException, ExecutionException {
+        return vaccinetakenService.getVaccineTakenById(id);
+    }
+
+    @GetMapping("/getVaccineTakensByType/{type}")
+    public List<VaccineTaken> getVaccineTakenByType(@PathVariable(value = "type") String type)
+            throws InterruptedException, ExecutionException {
+        return vaccinetakenService.getVaccineTakensByType(type);
+    }
+
+    @GetMapping("/getByStudentId/{studentId}")
+    // public VaccineTaken[] getVaccineTakenByStudentId(@RequestParam int studentId) throws
+    // InterruptedException, ExecutionException {
+    public List<VaccineTaken> getVaccineTakenByStudentId(
+            @PathVariable(value = "studentId") String studentId) throws InterruptedException, ExecutionException {
+        return vaccinetakenService.getVaccineTakensByStudentId(studentId);
+    }
+
+    @PutMapping("/update")
+    // public String updateVaccineTaken(@RequestParam VaccineTaken vaccinetaken) throws
+    // InterruptedException, ExecutionException {
+    public String updateVaccineTaken(@RequestBody VaccineTaken vaccinetaken) throws InterruptedException, ExecutionException {
+        return vaccinetakenService.updateVaccineTaken(vaccinetaken);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteVaccineTaken(@PathVariable(value = "id") String id) throws InterruptedException, ExecutionException {
+        return vaccinetakenService.deleteVaccineTaken(id);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> testGetEndPoint() {
+        return ResponseEntity.ok("Test get end-point is working.");
+    }
 }
--- src/main/java/com/infosaude/infosaude/services/VaccineTakenService.java
+++ src/main/java/com/infosaude/infosaude/services/VaccineTakenService.java
@@ -1,8 +1,115 @@
 package com.infosaude.infosaude.services;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.Query;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.cloud.FirestoreClient;
+import com.infosaude.infosaude.entities.VaccineTaken;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class VaccineTakenService {
+    public List<VaccineTaken> getAllVaccineTakens() throws InterruptedException, ExecutionException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        CollectionReference collection = dbFirestore.collection("vaccines-taken");
+
+        ApiFuture<QuerySnapshot> future = collection.get();
+        QuerySnapshot document = future.get();
+
+        if (!document.isEmpty()) {
+            List<VaccineTaken> vaccinetakens;
+            vaccinetakens = document.toObjects(VaccineTaken.class);
+            // toObject(VaccineTaken[].class);
+
+            return vaccinetakens;
+        }
+        return null;
+    }
+
+    public VaccineTaken getVaccineTakenById(String id) throws InterruptedException, ExecutionException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        DocumentReference documentReference = dbFirestore.collection("vaccines-taken").document(id);
+        ApiFuture<DocumentSnapshot> future = documentReference.get();
+        DocumentSnapshot document = future.get();
+
+        VaccineTaken vaccinetaken;
+        if (document.exists()) {
+            vaccinetaken = document.toObject(VaccineTaken.class);
+            return vaccinetaken;
+        }
+        return null;
+    }
+
+    public List<VaccineTaken> getVaccineTakensByType(String type) throws InterruptedException, ExecutionException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        Query docs = dbFirestore.collection("vaccines-taken").whereEqualTo("type", type);
+        // select("type", type);
+        ApiFuture<QuerySnapshot> future = docs.get();
+        QuerySnapshot document = future.get();
+
+        if (!document.isEmpty()) {
+            List<VaccineTaken> vaccinetakens;
+            vaccinetakens = document.toObjects(VaccineTaken.class);
+            // toObject(VaccineTaken[].class);
+
+            return vaccinetakens;
+        }
+        return null;
+    }
+
+    public List<VaccineTaken> getVaccineTakensByStudentId(String studentId) throws InterruptedException, ExecutionException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        Query docs = dbFirestore.collection("vaccines-taken").whereEqualTo("studentId", studentId);
+        // select("type", type);
+        ApiFuture<QuerySnapshot> future = docs.get();
+        QuerySnapshot document = future.get();
+
+        if (!document.isEmpty()) {
+            List<VaccineTaken> vaccinetakens;
+            vaccinetakens = document.toObjects(VaccineTaken.class);
+            // toObject(VaccineTaken[].class);
+
+            return vaccinetakens;
+        }
+        return null;
+    }
+
+    public String createVaccineTaken(VaccineTaken vaccinetaken) throws InterruptedException, ExecutionException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+
+        String id = dbFirestore.collection("vaccines-taken").document().getId();
+        vaccinetaken.setId(id);
+        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection("vaccines-taken").document(id).set(vaccinetaken);
+
+        // .add(vaccinetaken); => direto na collection, ou:
+        // dbFirestore.collection("vaccinetakens").document(id).set(vaccinetaken);
+
+        // create(vaccinetaken);
+        return collectionsApiFuture.get().toString();
+    }
+
+    public String updateVaccineTaken(VaccineTaken vaccinetaken) throws InterruptedException, ExecutionException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> writeResult = dbFirestore.collection("vaccines-taken").document(vaccinetaken.getId()).set(vaccinetaken);
+
+        return writeResult.get().getUpdateTime().toString();
+    }
+
+    public String deleteVaccineTaken(String id) throws InterruptedException, ExecutionException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> writeResult = dbFirestore.collection("vaccines-taken").document(id).delete();
+
+        String result = writeResult.get().toString();
+        return result;
+    }
     
 }
